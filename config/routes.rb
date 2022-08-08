6ef6aecbb0b6b94844461764d375627e58bd7e28
alvaro__barrera <alvaro_bv@hotmail.com>
2022-08-08 16:52:06 -0500

    Added layout navigation and root redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :plants
   resources :people
+  root to: "plants#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
